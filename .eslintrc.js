d036ca5c51f85d91a0e3c443a0daa8dc438af144
Jinwook <jinwook2765@naver.com>
2024-08-19 17:54:28 +0900

    Feat: Eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     // Prettier 규칙 사용
     'prettier/prettier': 'error',
+
     // React 관련 규칙
     'react/react-in-jsx-scope': 'off',
     'react/prefer-stateless-function': 'off',
@@ -34,10 +35,32 @@ module.exports = {
         unnamedComponents: 'arrow-function',
       },
     ],
+
     // TypeScript 관련 규칙
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // Import 관련 규칙
+    'import/no-unresolved': 'error', // 경로가 존재하지 않는 모듈 import 금지
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
   extends: ['airbnb', 'prettier'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'], // 파일 확장자 해석
+        moduleDirectory: ['node_modules', 'src/'], // 모듈 해석 경로 설정
+      },
+    },
+  },
 };
