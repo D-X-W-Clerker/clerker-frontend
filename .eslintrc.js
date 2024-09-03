07e37f514e1dbca504089d932bc278938f3baf9b
Jinwook <jinwook2765@naver.com>
2024-09-03 19:51:39 +0900

    feat: EsLint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 const globals = require('globals');
 
 module.exports = {
+  // ESLint가 인식할 환경을 설정합니다.
+  env: {
+    browser: true,
+    node: true,
+  },
+  // 기본으로 사용할 규칙 세트를 설정합니다.
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser', // 올바르게 설정된 parser
   parserOptions: {
     ecmaFeatures: {
@@ -9,14 +21,14 @@ module.exports = {
     project: './tsconfig.json',
   },
   globals: globals.browser,
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'react'],
   rules: {
     // Prettier 규칙 사용
     'prettier/prettier': 'error',
 
     // React 관련 규칙
-    'react/react-in-jsx-scope': 'off',
-    'react/prefer-stateless-function': 'off',
+    'react/react-in-jsx-scope': 'error',
+    'react/prefer-stateless-function': 'error',
     'react/jsx-filename-extension': [
       'error',
       {
@@ -26,10 +38,10 @@ module.exports = {
     'react/jsx-one-expression-per-line': 'off',
     'no-nested-ternary': 'off',
     'react/prop-types': 'off',
-    'import/no-extraneous-dependencies': 'off',
+    'import/no-extraneous-dependencies': 'error',
     'arrow-body-style': ['error', 'always'],
     'react/function-component-definition': [
-      'off',
+      'error',
       {
         namedComponents: 'arrow-function',
         unnamedComponents: 'arrow-function',
@@ -37,12 +49,13 @@ module.exports = {
     ],
 
     // TypeScript 관련 규칙
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
 
     // Import 관련 규칙
     'import/no-unresolved': 'error', // 경로가 존재하지 않는 모듈 import 금지
+    // import 구문에서 아래 파일 확장자를 사용하는 것을 금지
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -54,7 +67,6 @@ module.exports = {
       },
     ],
   },
-  extends: ['airbnb', 'prettier'],
   settings: {
     'import/resolver': {
       node: {
