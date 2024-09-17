d2673ea9a29ed07690845f2699750a89387ef33a
wngktjd13 <seonguk3553@kookmin.ac.kr>
2024-09-17 13:08:50 +0900

    feat: TutorialDetailPage 퍼블리싱

--- src/components/file/index.ts
+++ src/components/file/index.ts
@@ -1,1 +1,2 @@
 export { default as EventFile } from './EventFile';
+export { default as ContentDetail } from './ContentDetail';
---
+++ src/pages/TutorialDetailPage.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ContentDetail } from '@components';
+import { FlexCol, FlexRow } from '@styles';
+import Layout from '../Layout';
+
+const Container = styled(FlexCol)`
+  width: 100%;
+  max-width: 1100px;
+  align-items: center;
+  padding: 0;
+`;
+
+const TutorialDetailArea = styled(FlexRow)`
+  width: 1016px;
+  height: 595px;
+  margin: 35px 45px;
+`;
+
+const TutorialDetailPage: React.FC = () => {
+  const tutorialKey = 'CreateProject'; // 페이지에 따라 이 값을 동적으로 설정
+
+  return (
+    <Layout>
+      <ContentDetail tutorialKey={tutorialKey} />
+    </Layout>
+  );
+};
+
+export default TutorialDetailPage;
