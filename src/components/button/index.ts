dd04a1cdf1d69d612a6cdaf3658b2ebdff413d55
wngktjd13 <seonguk3553@kookmin.ac.kr>
2024-09-22 01:04:47 +0900

    fix : components error 해결

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -2,3 +2,4 @@ export { default as AddSubFolderButton } from './AddSubFolderButton';
 export { default as MeetCreateButton } from './MeetCreateButton';
 export { default as MemberAddButton } from './MemberAddButton';
 export { default as ModalButton } from './ModalButton';
+export { default as TutorialButton } from './TutorialButton';
--- src/components/file/index.ts
+++ src/components/file/index.ts
@@ -1,2 +1,3 @@
 export { default as EventFile } from './EventFile';
 export { default as MeetSummaryFile } from './MeetSummaryFile';
+export { default as ContentDetail } from './ContentDetail';
