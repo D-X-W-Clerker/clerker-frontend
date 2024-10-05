75544f3123fab66003ce5ba80a214cf4862440a9
Jinwook <jinwook2765@naver.com>
2024-10-05 20:31:13 +0900

    feat: 웰컴 모달 퍼블리싱

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -2,5 +2,6 @@ export { default as AccountSettingModal } from './AccountSettingModal';
 export { default as FolderModal } from './FolderModal';
 export { default as SmallModal } from './SmallModal';
 export { default as TutorialButton } from './TutorialModal';
+export { default as WelcomeModal } from './WelcomeModal';
 
 export * from './member';
