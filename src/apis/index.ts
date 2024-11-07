a5abbecf442c1e93d8213a2d62274c4d248f53a9
hyeonjin6530 <cola1876@naver.com>
2024-11-07 16:24:10 +0900

    feat: when2meet 시간 전송 api 연동

---
+++ src/apis/index.ts
@@ -0,0 +1,1 @@
+export { postTimeTable, getTimeTable } from './schedule';
--- src/apis/schedule.ts
+++ src/apis/schedule.ts
@@ -1,4 +1,41 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL;
+interface ScheduleRequest {
+    timeTable: string[];
+}
+
+const apiUrl = process.env.REACT_APP_BASE_URL;
+
+export const postTimeTable = async (
+    scheduleID: number,
+    data: ScheduleRequest,
+): Promise<void> => {
+    try {
+        const response = await axios.post(
+            `${apiUrl}/api/schedule/${scheduleID}`,
+            data,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            },
+        );
+        return response.data;
+    } catch (error) {
+        throw new Error(`timeTable 보내기에 실패했습니다: ${error}`);
+    }
+};
+
+export const getTimeTable = async (
+    projectID: number,
+    scheduleID: number,
+): Promise<void> => {
+    try {
+        const response = await axios.get(
+            `${apiUrl}/api/schedule/${projectID}/detail/${scheduleID}`,
+        );
+        return response.data;
+    } catch (error) {
+        throw new Error(`timeTable 가져오기에 실패했습니다: ${error}`);
+    }
+};
