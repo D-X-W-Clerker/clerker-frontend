c22e2b319addb8fd36e265919e3404ef161b63a0
Jinwook <jinwook2765@naver.com>
2025-01-01 14:05:18 +0900

    refactor: IntroSection 배경 이미지 추가

--- src/assets/onboarding/index.ts
+++ src/assets/onboarding/index.ts
@@ -2,10 +2,12 @@ import FeatureSectionImage from './FeatureSectionImage.svg';
 import SummarySectionImage from './SummarySectionImage.svg';
 import When2meetSectionImage from './When2meetSectionImage.svg';
 import ProjectSectionImage from './ProjectSectionImage.svg';
+import IntroSectionBg from './IntroSectionBg.png';
 
 export {
     FeatureSectionImage,
     SummarySectionImage,
     When2meetSectionImage,
     ProjectSectionImage,
+    IntroSectionBg,
 };
