43f2b633e041de4ecf66cdfeefce72332359fd7d
Jinwook <jinwook2765@naver.com>
2025-01-19 21:44:51 +0900

    refactor: 스타일 간소화 및 시간 관리 상태 최적화

--- src/components/modal/schedule/ScheduleCreateModal.tsx
+++ src/components/modal/schedule/ScheduleCreateModal.tsx
@@ -1,14 +1,19 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { DownArrowIcon } from '@assets';
 import {
     LargeModalTitleTab,
     ProjectInput,
     ModalButton,
     RadioInput,
 } from '@components';
-import { CenterRow, FlexCol, ItemsCenterSpaceRow } from '@styles';
-import DownArrowIcon from '../../../assets/action/arrow/DownArrowIcon.svg';
+import {
+    CenterRow,
+    FlexCol,
+    ItemsCenterEndRow,
+    ItemsCenterSpaceRow,
+} from '@styles';
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
@@ -53,13 +58,13 @@ const Container = styled(FlexCol)`
     height: 300px;
     box-sizing: border-box;
     gap: 20px;
-    padding: 20px 20px;
+    padding: 20px;
     background-color: var(--background-color);
     border-radius: 10px;
-    position: relative;
 `;
 
 const ContentArea = styled(FlexCol)`
+    flex: 1;
     gap: 10px;
 `;
 
@@ -68,11 +73,7 @@ const DateInputArea = styled(ItemsCenterSpaceRow)`
     gap: 10px;
 `;
 
-const ButtonArea = styled.div`
-    position: absolute;
-    bottom: 20px;
-    right: 20px;
-    display: flex;
+const ButtonArea = styled(ItemsCenterEndRow)`
     gap: 10px;
 `;
 
@@ -85,12 +86,11 @@ const StyledSelect = styled.select`
     border: none;
     background-color: var(--color-gray-50);
     font-size: inherit;
-    padding: 5px 10px 5px 0;
+    padding: 5px 34px 5px 0;
     width: 100%;
     border-radius: 4px;
     text-align: right;
     appearance: none;
-    padding-right: 24px;
 
     &:focus {
         outline: none;
@@ -127,10 +127,10 @@ const ScheduleCreateModal: React.FC<ScheduleCreateModalProps> = ({
     endDate,
 }) => {
     const [title, setTitle] = useState<string>('');
-    const [startHour, setStartHour] = useState<string>('09');
-    const [startMinute, setStartMinute] = useState<string>('00');
-    const [endHour, setEndHour] = useState<string>('10');
-    const [endMinute, setEndMinute] = useState<string>('00');
+    const [time, setTime] = useState({
+        start: { hour: '', minute: '' },
+        end: { hour: '', minute: '' },
+    });
     const [sendAlert, setSendAlert] = useState<boolean>(false);
 
     const onChangeInput = (
@@ -139,28 +139,17 @@ const ScheduleCreateModal: React.FC<ScheduleCreateModalProps> = ({
         setTitle(event.target.value);
     };
 
-    const handleStartHourChange = (
-        e: React.ChangeEvent<HTMLSelectElement>,
+    const onChangeTime = (
+        key: 'start' | 'end',
+        field: 'hour' | 'minute',
+        value: string,
     ): void => {
-        setStartHour(e.target.value);
-    };
-
-    const handleStartMinuteChange = (
-        e: React.ChangeEvent<HTMLSelectElement>,
-    ): void => {
-        setStartMinute(e.target.value);
-    };
-
-    const handleEndHourChange = (
-        e: React.ChangeEvent<HTMLSelectElement>,
-    ): void => {
-        setEndHour(e.target.value);
-    };
-
-    const handleEndMinuteChange = (
-        e: React.ChangeEvent<HTMLSelectElement>,
-    ): void => {
-        setEndMinute(e.target.value);
+        setTime((prev) => {
+            return {
+                ...prev,
+                [key]: { ...prev[key], [field]: value },
+            };
+        });
     };
 
     const onClickCreateButton = async (): Promise<void> => {
@@ -170,8 +159,8 @@ const ScheduleCreateModal: React.FC<ScheduleCreateModalProps> = ({
         }
 
         // 시간 데이터 구성 (시간:분:초 형식)
-        const formattedStartTime = `${startHour}:${startMinute}:00`;
-        const formattedEndTime = `${endHour}:${endMinute}:00`;
+        const formattedStartTime = `${time.start.hour}:${time.start.minute}:00`;
+        const formattedEndTime = `${time.end.hour}:${time.end.minute}:00`;
 
         const newScheduleData = {
             name: title,
@@ -198,6 +187,17 @@ const ScheduleCreateModal: React.FC<ScheduleCreateModalProps> = ({
         }
     };
 
+    const timeFields: {
+        key: 'start' | 'end';
+        field: 'hour' | 'minute';
+        options: { label: string; value: string }[];
+    }[] = [
+        { key: 'start', field: 'hour', options: hourOptions },
+        { key: 'start', field: 'minute', options: minuteOptions },
+        { key: 'end', field: 'hour', options: hourOptions },
+        { key: 'end', field: 'minute', options: minuteOptions },
+    ];
+
     return (
         <Backdrop>
             <Container>
@@ -210,79 +210,39 @@ const ScheduleCreateModal: React.FC<ScheduleCreateModalProps> = ({
                         placeholder="일정 제목을 입력하세요."
                     />
                     <DateInputArea>
-                        <StyledSelectWrapper>
-                            <StyledSelect
-                                value={startHour}
-                                onChange={handleStartHourChange}
-                            >
-                                {hourOptions.map((option) => {
-                                    return (
-                                        <option
-                                            key={option.value}
-                                            value={option.value}
+                        {timeFields.map(({ key, field, options }, index) => {
+                            return (
+                                <>
+                                    <StyledSelectWrapper
+                                        key={`${key}-${field}`}
+                                    >
+                                        <StyledSelect
+                                            value={time[key][field]}
+                                            onChange={(e) => {
+                                                return onChangeTime(
+                                                    key as 'start' | 'end',
+                                                    field as 'hour' | 'minute',
+                                                    e.target.value,
+                                                );
+                                            }}
                                         >
-                                            {option.label}
-                                        </option>
-                                    );
-                                })}
-                            </StyledSelect>
-                            <ArrowIcon src={DownArrowIcon} />
-                        </StyledSelectWrapper>
-                        <StyledSelectWrapper>
-                            <StyledSelect
-                                value={startMinute}
-                                onChange={handleStartMinuteChange}
-                            >
-                                {minuteOptions.map((option) => {
-                                    return (
-                                        <option
-                                            key={option.value}
-                                            value={option.value}
-                                        >
-                                            {option.label}
-                                        </option>
-                                    );
-                                })}
-                            </StyledSelect>
-                            <ArrowIcon src={DownArrowIcon} />
-                        </StyledSelectWrapper>
-                        <span>~</span>
-                        <StyledSelectWrapper>
-                            <StyledSelect
-                                value={endHour}
-                                onChange={handleEndHourChange}
-                            >
-                                {hourOptions.map((option) => {
-                                    return (
-                                        <option
-                                            key={option.value}
-                                            value={option.value}
-                                        >
-                                            {option.label}
-                                        </option>
-                                    );
-                                })}
-                            </StyledSelect>
-                            <ArrowIcon src={DownArrowIcon} />
-                        </StyledSelectWrapper>
-                        <StyledSelectWrapper>
-                            <StyledSelect
-                                value={endMinute}
-                                onChange={handleEndMinuteChange}
-                            >
-                                {minuteOptions.map((option) => {
-                                    return (
-                                        <option
-                                            key={option.value}
-                                            value={option.value}
-                                        >
-                                            {option.label}
-                                        </option>
-                                    );
-                                })}
-                            </StyledSelect>
-                            <ArrowIcon src={DownArrowIcon} />
-                        </StyledSelectWrapper>
+                                            {options.map((option) => {
+                                                return (
+                                                    <option
+                                                        key={option.value}
+                                                        value={option.value}
+                                                    >
+                                                        {option.label}
+                                                    </option>
+                                                );
+                                            })}
+                                        </StyledSelect>
+                                        <ArrowIcon src={DownArrowIcon} />
+                                    </StyledSelectWrapper>
+                                    {index === 1 && <span>~</span>}
+                                </>
+                            );
+                        })}
                     </DateInputArea>
                     <RadioInput
                         label="멤버들에게 일정 생성 알림을 보낼까요?"
