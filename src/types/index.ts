4c5d19383ae21ac27f69fef4742c9278dacdd9f2
Jinwook <jinwook2765@naver.com>
2025-02-06 11:24:11 +0900

    refactor: 인터페이스 인덱싱 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,7 @@
+export * from './button';
+export * from './input';
+export * from './meeting';
 export * from './project';
+export * from './schedule';
+export * from './titleTab';
+export * from './user';
